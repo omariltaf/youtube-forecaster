23aa20e218a864070966660cb708e4b5a6935218
Omar Iltaf <omariltaf@gmail.com>
2021-03-28 22:09:20 +0100

    Update html element names

--- popup.js
+++ popup.js
@@ -1,18 +1,18 @@
 // Checking if the extension should be active on the current page
 chrome.storage.local.get(["allowForecasting"], function (storage) {
   if (storage.allowForecasting) {
-    document.getElementById("forecast").style.display = "";
-    document.getElementById("result").innerHTML = "";
+    document.getElementById("doForecast").style.display = "";
+    document.getElementById("status").innerHTML = "";
   } else {
-    document.getElementById("forecast").style.display = "none";
-    document.getElementById("result").innerHTML = "Go to a Youtube Video page";
+    document.getElementById("doForecast").style.display = "none";
+    document.getElementById("status").innerHTML = "Go to a Youtube Video page";
   }
 });
 
 // Logic for button
-document.getElementById("forecast").addEventListener("click", () => {
-  document.getElementById("result").innerHTML = "Forecasting...";
-  document.getElementById("forecast").style.display = "none";
+document.getElementById("doForecast").addEventListener("click", () => {
+  document.getElementById("status").innerHTML = "Forecasting...";
+  document.getElementById("doForecast").style.display = "none";
 
   // Start Forecasting
   chrome.runtime.sendMessage({ message: "startForecast" }, (response) => {
@@ -30,13 +30,13 @@ document.getElementById("forecast").addEventListener("click", () => {
       );
       let endText = document.createTextNode(" is on: ");
 
-      document.getElementById("result").innerHTML = "";
-      document.getElementById("result").appendChild(startText);
-      document.getElementById("result").appendChild(channelTitle);
-      document.getElementById("result").appendChild(endText);
-      document.getElementById("result").appendChild(dummyForecastDate);
+      document.getElementById("status").innerHTML = "";
+      document.getElementById("status").appendChild(startText);
+      document.getElementById("status").appendChild(channelTitle);
+      document.getElementById("status").appendChild(endText);
+      document.getElementById("status").appendChild(dummyForecastDate);
     } else {
-      document.getElementById("result").innerHTML = response.error;
+      document.getElementById("status").innerHTML = response.error;
     }
   });
 });
