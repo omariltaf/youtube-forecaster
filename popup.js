3179687927834826b642b8af91c24a3dd1b6af45
Omar Iltaf <omariltaf@gmail.com>
2021-04-24 23:13:21 +0100

    Update popup script

--- popup.js
+++ popup.js
@@ -1,18 +1,22 @@
 // Checking if the extension should be active on the current page
 chrome.storage.local.get(["allowForecasting"], function (storage) {
   if (storage.allowForecasting) {
-    document.getElementById("doForecast").style.display = "";
-    document.getElementById("status").innerHTML = "";
+    showForecastButton();
+    hideForecastCard();
   } else {
-    document.getElementById("doForecast").style.display = "none";
-    document.getElementById("status").innerHTML = "Go to a Youtube Video page";
+    hideForecastButton();
+    showForecastCard();
+    document.getElementById("forecastCard").innerHTML =
+      "Go to a Youtube Video page";
   }
 });
 
 // Logic for button
-document.getElementById("doForecast").addEventListener("click", () => {
+document.getElementById("forecastButton").addEventListener("click", () => {
   document.getElementById("status").innerHTML = "Forecasting...";
-  document.getElementById("doForecast").style.display = "none";
+  hideForecastButton();
+  showForecastCard();
+  // document.getElementById("forecastButton").classList.toggle("transform-active");
 
   // Start Forecasting
   chrome.runtime.sendMessage({ message: "startForecast" }, (response) => {
@@ -33,10 +37,26 @@ document.getElementById("doForecast").addEventListener("click", () => {
       document.getElementById("status").innerHTML = "";
       document.getElementById("status").appendChild(channelTitle);
 
-      document.getElementById("forecastInner").innerHTML =
+      document.getElementById("forecastCard").innerHTML =
         "Most frequent upload days: " + response.forecast.days;
     } else {
       document.getElementById("status").innerHTML = response.error;
     }
   });
 });
+
+function hideForecastButton() {
+  document.getElementById("forecastButton").style.display = "none";
+}
+
+function showForecastButton() {
+  document.getElementById("forecastButton").style.display = "";
+}
+
+function hideForecastCard() {
+  document.getElementById("forecastCard").style.display = "none";
+}
+
+function showForecastCard() {
+  document.getElementById("forecastCard").style.display = "";
+}
