38189aa23cd3ea60e8f6627788484319bc847a06
Omar Iltaf <omariltaf@gmail.com>
2021-05-02 19:06:16 +0100

    Refactor frontend

--- popup.js
+++ popup.js
@@ -1,71 +1,62 @@
 // Checking if the extension should be active on the current page
 chrome.storage.local.get(["allowForecasting"], function (storage) {
   if (storage.allowForecasting) {
-    showForecastButton();
-    hideForecastCard();
+    shouldShowElementById("forecastResults", false);
+    shouldShowElementById("forecastMessages", false);
+    shouldShowElementById("forecastButton", true);
   } else {
-    hideForecastButton();
-    showForecastCard();
-    document.getElementById("status").innerHTML = "Go to a Youtube Video page";
+    shouldShowElementById("forecastButton", false);
+    shouldShowElementById("forecastResults", false);
+    shouldShowElementById("forecastMessages", true);
+    outputForecastMessages("Go to a YouTube Video page");
   }
 });
 
-// Logic for button
+// Button Logic
 document.getElementById("forecastButton").addEventListener("click", () => {
-  hideForecastButton();
-  showLoader();
-  setTimeout(startForecast, 2000);
+  shouldShowElementById("forecastButton", false);
+  shouldShowElementById("forecastLoader", true);
+  setTimeout(startForecast, 0);
 });
 
 function startForecast() {
   chrome.runtime.sendMessage({ message: "startForecast" }, (response) => {
-    hideLoader();
-    showForecastCard();
+    shouldShowElementById("forecastLoader", false);
     if (response.successful) {
-      let channelTitle = document.createElement("SPAN");
-      channelTitle.style.color = "blue";
-      channelTitle.innerHTML = response.channelTitle;
-
-      let dummyForecastDate = document.createElement("SPAN");
-      dummyForecastDate.style.color = "green";
-      dummyForecastDate.innerHTML = "06/03/2021";
-
-      let startText = document.createTextNode(
-        "Forecasted next upload date for "
-      );
-      let endText = document.createTextNode(" is on: ");
-
-      document.getElementById("status").innerHTML = response.channelTitle;
+      shouldShowElementById("forecastResults", true);
+      outputForecastResults(response);
+      // let channelTitle = document.createElement("SPAN");
+      // channelTitle.style.color = "blue";
+      // channelTitle.innerHTML = response.channelTitle;
+      // let dummyForecastDate = document.createElement("SPAN");
+      // dummyForecastDate.style.color = "green";
+      // dummyForecastDate.innerHTML = "06/03/2021";
+      // let startText = document.createTextNode(
+      //   "Forecasted next upload date for "
+      // );
+      // let endText = document.createTextNode(" is on: ");
+      // document.getElementById("status").innerHTML = response.channelTitle;
       // document.getElementById("status").appendChild(channelTitle);
-
-      // document.getElementById("forecastCard").innerHTML =
+      // document.getElementById("forecastResults").innerHTML =
       //   "Most frequent upload days: " + response.forecast.days;
     } else {
-      document.getElementById("status").innerHTML = response.error;
+      shouldShowElementById("forecastMessages", true);
+      outputForecastMessages(response.error);
     }
   });
 }
 
-function hideForecastButton() {
-  document.getElementById("forecastButton").style.display = "none";
+function outputForecastResults(response) {
+  document.getElementById("forecastDisplay").textContent =
+    response.channelTitle;
 }
 
-function showForecastButton() {
-  document.getElementById("forecastButton").style.display = "";
+function outputForecastMessages(forecastMessage) {
+  document.getElementById("forecastMessages").textContent = forecastMessage;
 }
 
-function hideLoader() {
-  document.getElementById("forecastLoader").style.display = "none";
-}
-
-function showLoader() {
-  document.getElementById("forecastLoader").style.display = "block";
-}
-
-function hideForecastCard() {
-  document.getElementById("forecastCard").style.display = "none";
-}
-
-function showForecastCard() {
-  document.getElementById("forecastCard").style.display = "";
+function shouldShowElementById(elementId, shouldShow) {
+  document.getElementById(elementId).style.display = shouldShow
+    ? "block"
+    : "none";
 }
